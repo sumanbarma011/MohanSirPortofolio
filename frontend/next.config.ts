cc327aab9197f08655fda9f5a57eb9230d6e3a77
roshan393 <chanantony29@gmail.com>
2026-06-14 12:49:37 +0545

    starter files

---
+++ frontend/next.config.ts
@@ -0,0 +1,21 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "hireme-one.vercel.app",
+        port: "",
+        pathname: "/assets/images/**",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
+  },
+};
+
+export default nextConfig;

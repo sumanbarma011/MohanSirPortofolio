093b489edbd61a6a7e501e1f32faa1f026d01a30
roshan393 <chanantony29@gmail.com>
2026-06-15 14:25:07 +0545

    minor issue fix

--- frontend/app/(auth)/login/page.tsx
+++ frontend/app/(auth)/login/page.tsx
@@ -4,6 +4,7 @@ const Login = () => {
   return (
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm">
+        {/* login form */}
         <LoginForm />
       </div>
     </div>
